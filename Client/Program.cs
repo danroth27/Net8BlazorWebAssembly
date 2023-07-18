cd50cfde1a3dc48b2773be12f2236dd325ae9019
Daniel Roth <daroth@microsoft.com>
2023-07-17 19:05:20 -0700

    Update client Program.cs

--- Client/Program.cs
+++ Client/Program.cs
@@ -4,7 +4,6 @@ using Net8BlazorWebAssembly.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
